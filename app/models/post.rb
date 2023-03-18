607424545885c4301e6759b3c238e4393bcc9caf
YKarsten <yannik.karsten@web.de>
2023-03-18 13:01:17 +0100

    Set up Comment model and association with User and Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   validates :title, :body, presence: true
   validates :title, :body, length: { minimum: 3 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
+
   validates :name, :email, :password, presence: true
   validates :password, :name, length: { in: 4..20 }
 end
